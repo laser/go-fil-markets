99c2702487b168d9e55f8a54f8fff477dec3ff6b
shannonwells <shannonwells@users.noreply.github.com>
2020-06-03 17:17:43 -0700

    test harnesses for easier setup of integration tests

--- retrievalmarket/impl/provider.go
+++ retrievalmarket/impl/provider.go
@@ -93,6 +93,10 @@ func (p *provider) Stop() error {
 	if err != nil {
 		return err
 	}
+	err = p.stateMachines.Stop(context.Background())
+	if err != nil {
+		return err
+	}
 	return p.network.StopHandlingRequests()
 }
 
